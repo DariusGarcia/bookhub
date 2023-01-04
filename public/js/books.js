2a9a45af13658674e5911f5d291a7b5c81384438
Nathan Difiori <ndifiori@gmail.com>
2023-01-04 13:58:33 -0800

    updatebook javascript logic

--- public/js/books.js
+++ public/js/books.js
@@ -31,3 +31,35 @@ const bookForm = document.querySelector('#book-form')
 if (bookForm) {
   bookForm.addEventListener('submit', bookFormHandler)
 }
+
+
+const updateBookFormHandler = aysnc function (e) {
+  e.preventDefault()
+
+  const title = document.querySelector('#book-form-title-input').value
+  const author = document.querySelector('#book-form-author-input').value
+  const genre = document.querySelector('#book-form-genre-input').value
+  const description = document.querySelector('#book-form-description-input').value
+  const publishingDate = document.querySelector('#book-form-datePublished-input').value
+
+  await fetch ('/api/books', {
+    method: 'PUT',
+    body: JSON.stringify{(
+      title,
+      author,
+      genre,
+      description,
+      publishingDate
+    )}
+    headers: { 'Content-Type': 'application/json'},
+  })
+
+  document.location.replace('/')
+
+}
+
+const updateBookForm = document.querySelector('#update-book-form')
+
+if (updateBookForm) {
+  updateBookForm.addEventListener('submit', updateBookFormHandler)
+}
