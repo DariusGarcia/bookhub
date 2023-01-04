e3beee7e119002c4360ac7c927fd794e6d3b6929
Aaron Wilson <aaronmwilson96@gmail.com>
2023-01-04 13:32:55 -0900

    added: logout

--- controller/api/userRoutes.js
+++ controller/api/userRoutes.js
@@ -50,4 +50,13 @@ userRouter.post('/login', async (req, res) => {
   }
 })
 
+userRouter.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end()
+    })
+  } else {
+    res.status(404).end()
+  }
+})
 module.exports = userRouter
