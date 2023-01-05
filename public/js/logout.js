2c67d988a5ccc031ca101671dd808c793b8d7c3b
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-04 16:15:01 -0800

    added: alert when user successfully logs out

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,16 +1,17 @@
 const logout = async function (event) {
   event.preventDefault()
-  
+
   const response = await fetch('/api/user/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-  });
+  })
 
   if (response.ok) {
-    document.location.replace('/');
+    alert('Successfully logged out.')
+    document.location.replace('/')
   } else {
-    alert('Failed to log out');
+    alert('Failed to log out')
   }
-};
+}
 
-document.querySelector('#logout-link').addEventListener('click', logout);
+document.querySelector('#logout-link').addEventListener('click', logout)
